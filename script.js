75842716da51950550658aaf8305759e11b6a8ba
kise <kaiazmi1412@gmail.com>
2023-10-30 22:09:27 +0530

    feat: Added-Extra-MenuList

--- script.js
+++ script.js
@@ -12,106 +12,137 @@ const errorMessage = document.getElementById('error-message');
 let isIncome = true; // Flag to track whether the transaction is income or expense
 
 incomeBtn.addEventListener('click', () => {
-  isIncome = true;
-  incomeBtn.classList.add('active');
-  expenseBtn.classList.remove('active');
+	isIncome = true;
+	incomeBtn.classList.add('active');
+	expenseBtn.classList.remove('active');
 });
 
 expenseBtn.addEventListener('click', () => {
-  isIncome = false;
-  expenseBtn.classList.add('active');
-  incomeBtn.classList.remove('active');
+	isIncome = false;
+	expenseBtn.classList.add('active');
+	incomeBtn.classList.remove('active');
 });
 
-const localStorageTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
+const localStorageTransactions =
+	JSON.parse(localStorage.getItem('transactions')) || [];
 
 let transactions = localStorageTransactions;
 
 function addTransaction(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  const enteredText = text.value;
-  const enteredAmount = +amount.value;
+	const enteredText = text.value;
+	const enteredAmount = +amount.value;
 
-  if (enteredText.trim() === '' || isNaN(enteredAmount) || enteredAmount === 0) {
-    errorMessage.textContent = 'Please enter a valid item and amount.';
-  } else {
-    errorMessage.textContent = '';
+	if (
+		enteredText.trim() === '' ||
+		isNaN(enteredAmount) ||
+		enteredAmount === 0
+	) {
+		errorMessage.textContent = 'Please enter a valid item and amount.';
+	} else {
+		errorMessage.textContent = '';
 
-    const transaction = {
-      id: generateID(),
-      text: enteredText,
-      amount: isIncome ? enteredAmount : -enteredAmount,
-    };
+		const transaction = {
+			id: generateID(),
+			text: enteredText,
+			amount: isIncome ? enteredAmount : -enteredAmount,
+		};
 
-    transactions.push(transaction);
+		transactions.push(transaction);
 
-    addTransactionDOM(transaction);
-    updateValues();
-    updateLocalStorage();
+		addTransactionDOM(transaction);
+		updateValues();
+		updateLocalStorage();
 
-    text.value = '';
-    amount.value = '';
-  }
+		text.value = '';
+		amount.value = '';
+	}
 }
 
 function generateID() {
-  return '_' + Math.random().toString(36).substr(2, 9);
+	return '_' + Math.random().toString(36).substr(2, 9);
 }
 
 function addTransactionDOM(transaction) {
-  const sign = transaction.amount < 0 ? '-' : '+';
-  const item = document.createElement('li');
-  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-  item.innerHTML = `
+	const sign = transaction.amount < 0 ? '-' : '+';
+	const item = document.createElement('li');
+	item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+	item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
     <button class="delete-btn">x</button>
   `;
 
-  item.querySelector('.delete-btn').addEventListener('click', () => removeTransaction(transaction.id));
-  list.appendChild(item);
+	item
+		.querySelector('.delete-btn')
+		.addEventListener('click', () => removeTransaction(transaction.id));
+	list.appendChild(item);
 }
 
 list.addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete-btn')) {
-    const itemId = e.target.parentElement.dataset.id;
-    removeTransaction(itemId);
-  }
+	if (e.target.classList.contains('delete-btn')) {
+		const itemId = e.target.parentElement.dataset.id;
+		removeTransaction(itemId);
+	}
 });
 
 function updateValues() {
-  const amounts = transactions.map(transaction => transaction.amount);
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
-  const expense = Math.abs(amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0)).toFixed(2);
+	const amounts = transactions.map((transaction) => transaction.amount);
+	const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+	const income = amounts
+		.filter((item) => item > 0)
+		.reduce((acc, item) => (acc += item), 0)
+		.toFixed(2);
+	const expense = Math.abs(
+		amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0)
+	).toFixed(2);
 
-  balance.innerText = `$${total}`;
-  moneyPlus.innerText = `$${income}`;
-  moneyMinus.innerText = `$${expense}`;
+	balance.innerText = `$${total}`;
+	moneyPlus.innerText = `$${income}`;
+	moneyMinus.innerText = `$${expense}`;
 }
 
 function removeTransaction(id) {
-  transactions = transactions.filter(transaction => transaction.id !== id);
-  updateLocalStorage();
-  init();
+	transactions = transactions.filter((transaction) => transaction.id !== id);
+	updateLocalStorage();
+	init();
 }
 
 function updateLocalStorage() {
-  localStorage.setItem('transactions', JSON.stringify(transactions));
+	localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
-const items = ['Gas', 'Electric', 'Grocery', 'Internet'];
+const items = [
+	'Budget',
+	'Gas',
+	'Electricity',
+	'Groceries',
+	'Internet',
+	'Rent',
+	'Water Bill',
+	'Phone Bill',
+	'Car Insurance',
+	'Health Insurance',
+	'Public Transport',
+	'Eating Out',
+	'Entertainment',
+	'Clothing',
+	'Gym Membership',
+	'Education',
+	'Medical Expenses',
+	'Miscellaneous',
+];
 
 function init() {
-  list.innerHTML = '';
-  items.forEach(item => {
-    const option = document.createElement('option');
-    option.value = item;
-    option.textContent = item;
-    text.appendChild(option);
-  });
-  transactions.forEach(addTransactionDOM);
-  updateValues();
+	list.innerHTML = '';
+	items.forEach((item) => {
+		const option = document.createElement('option');
+		option.value = item;
+		option.textContent = item;
+		text.appendChild(option);
+	});
+	transactions.forEach(addTransactionDOM);
+	updateValues();
 }
 
 init();
